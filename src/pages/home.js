6379dcd6aaa3ceef575f5dee10b74682620ac45a
lucaslimasz <id.lucas@outlook.com.br>
2021-12-28 23:53:43 -0300

    axios config

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,76 @@
-import * as S from './styles';
-import Header from '../components/Header';
+import { useState, useEffect, useRef } from "react";
+
+import * as S from "./styles";
+import api from "../config/api";
+
+import Header from "../components/Header";
+import Storys from "../components/Storys";
+import Post from "../components/Post";
+
+import Config from "../assets/perfil.png";
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasEndingPosts, setHasEndingPosts] = useState(false);
+  const loaderRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver((entities) => {
+      const target = entities[0];
+
+      if (target.isIntersecting) {
+        setCurrentPage((prevState) => prevState + 1);
+      }
+    }, options);
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get("posts", {
+        params: {
+          _page: currentPage,
+          _limit: 3,
+        },
+      });
+
+      if (!data.length) {
+        setHasEndingPosts(true);
+      }
+
+      setPosts([...posts, ...data]);
+    })();
+  }, [currentPage]);
+
   return (
     <S.Container>
       <Header />
+
+      <S.Body>
+        <Storys />
+        {posts.map((item) => (
+          <Post
+            key={item.id}
+            perfil={item.perfil}
+            user_name={item.user_name}
+            post={item.post}
+          />
+        ))}
+        <img src={Config} alt="" />
+      </S.Body>
+      {!hasEndingPosts && <p ref={loaderRef}>CARREGANDO....</p>}
     </S.Container>
-  )
+  );
 }
 
 export default Home;
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,5 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
-  
+export const Container = styled.div``;
+
+export const Body = styled.div`
+  padding: 30px;
+  max-width: 1010px;
+  margin: auto;
+
+  display: grid;
+  column-gap: 29px;
+  grid-template-columns: 616px 300px;
+
+  > img {
+    grid-column: 2/3;
+    grid-row: 1/3;
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 100%;
+    > img {
+      display: none;
+    }
+  }
 `;
